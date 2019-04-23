3809585da5980f6dbe054f5da149e0d365a97951
SamratAgl <Samrat.sarkar1@tcs..com>
2019-04-24 00:50:15 +0530

    Test Framework Changes

---
+++ AGL.WebAPI/Models/People.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AGL.WebAPI.Models
+{
+    public class People
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("gender")]
+        public string Gender { get; set; }
+
+        [JsonProperty("age")]
+        public int Age { get; set; }
+
+        [JsonProperty("pets")]
+        public List<PetDetails> Pets { get; set; }
+    }
+}
